6720b6e09b5a7ce4887f3afefc0e84052194cf43
Arpit Solanki <solankiarpit1997@gmail.com>
2016-03-08 22:45:11 +0530

    added support for unicode(UTF-8) characters

--- TextAsWallpaper.py
+++ TextAsWallpaper.py
@@ -7,16 +7,23 @@ background = input('Enter background colour ')
 text_colour = input('Enter Text colour ')
 text_stroke = input('Enter Text stroke ')
 font_name = input('Enter font name ')
+font_size = input('Enter font size(px) ') 
 svg_template1 = '''<svg height="1080" width="1920" >
 	<rect x="0" y="0" height="1080" width="1920" style="fill:{};stroke:black" />
 	<text x="960" y="540" 
-		text-anchor="middle" font-weight="bold" font-size="30px" 
+		text-anchor="middle" font-weight="bold" font-size="{}" 
 		lengthAdjust="spacingAndGlyphs" 
 		style="fill:{};stroke:{};font-family: {} font-weight: normal; font-style: normal" >
 		{}
 	</text>
 </svg>'''
-svg_template1 = svg_template1.format(background,text_colour,text_stroke,font_name,string_quote)
+s = []
+for i in string_quote:
+	s.append('&#')
+	s.append(str(ord(i)))
+	s.append(';')
+quote = ''.join(s)
+svg_template1 = svg_template1.format(background,font_size,text_colour,text_stroke,font_name,quote)
 svg = open('wallpaper.svg','w')
 svg.write(svg_template1)
 svg.close() 
